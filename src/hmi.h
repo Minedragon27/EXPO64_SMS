dcd4e0b84b31a860cc6dbf070b33f27c17367c9e
Minedragon27 <iivanov0449@gmail.com>
2025-05-29 15:26:05 +0200

    added drawText() for drawing centered text and writeToScreen() for showing the data

--- src/hmi.h
+++ src/hmi.h
@@ -1,17 +1,21 @@
 #include <Arduino.h>
+#include <TFT.h>
+#include <SPI.h>
 
 class HMI
 {
     enum parameter {none=0,temperature, humidity, co2, light};
     public: parameter currentParameter=none;
 
+    private: TFT &screen;
     private: short resistances[8];//resistances for the button circuit
     private: int pinA,pinB,pinC,pinButtons;
     private: int pinLED[4];//led pins
 
+
     
 
-    HMI(int A,int B, int C, int Buttons, short R[8], int LED[4])
+    HMI(int A,int B, int C, int Buttons, short R[8], int LED[4],TFT &TFTscreen): screen(TFTscreen)
     {
         pinA=A;//rotary enc A
         pinMode(A,INPUT);
@@ -23,7 +27,7 @@ class HMI
         pinMode(Buttons,INPUT);
         memcpy(resistances, R, sizeof(R));//resistances in Ohms, code coppies R into resistances
         memcpy(pinLED,LED,sizeof(LED));
-
+        screen=TFTscreen;
     }
 
     void readRotaryEncoder(bool* ptrA,bool* ptrB)
@@ -49,11 +53,39 @@ class HMI
         }
         return output;
     }
+    
     void setLEDs(byte LEDs)
     {
         for(int i=0;i<4;i++) digitalWrite(pinLED[i],readBit(LEDs,i));//writes each bit to its corresponding LED pin
     }
-    
+
+    void writeToScreen(byte currentValue, byte targetValue)
+    {   
+        String unit="";
+        switch (currentParameter)//chooses the unit to be displayed
+        {
+        case 1:
+            unit="C";
+            break;
+        case 2:
+            unit="%";
+            break;
+        case 3:
+            unit="ppm";
+            break;
+        case 4:
+            unit="lux";
+            break;    
+        default:
+            break;
+        }
+
+
+        drawText("Current:",3,5);
+        drawText(String(currentValue)+unit,2,10+3*8);
+        drawText("Target:",3,screen.height()/2+5);
+        drawText(String(targetValue)+unit,2,screen.height()/2+5+10+3*8);
+    }
 
     bool readBit(byte variable, int position) 
     {
@@ -63,5 +95,15 @@ class HMI
     {
         variable = (variable & ~(1 << position)) | ((bitValue & 1) << position);
     }
+    void drawText( const String &text, int textSize, int y) //draws horizontally centered text
+    {
+        char buffer[text.length() + 1];
+        text.toCharArray(buffer, sizeof(buffer));
 
+        int textWidth = text.length() * 6 * textSize;
+        int x = (screen.width() - textWidth) / 2;
+
+        screen.setTextSize(textSize);
+        screen.text(buffer, x, y);
+    }
 };
