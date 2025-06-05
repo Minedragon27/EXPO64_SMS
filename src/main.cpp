2088c4ace700ac81c1ffd1ada0813f34e0db60af
thesemsea <isabelasanunes@gmail.com>
2025-06-05 14:18:50 +0200

    removed sample code

--- src/main.cpp
+++ src/main.cpp
@@ -99,14 +99,11 @@ void UpdateTargetParameters()
     }
 
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
   // put your setup code here, to run once:
-  
+    //
 
-}
+}   
 
 void loop() {
   // put your main code here, to run repeatedly:
