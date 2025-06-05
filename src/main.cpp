6ed28b8d05e1c773384eba10b6be92b80ec243df
Minedragon27 <iivanov0449@gmail.com>
2025-06-05 13:24:40 +0200

    added the logic for each of the menu loops

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,22 @@ short R[8]={5,6,7,8,9,10,11,12};
 int LED[4]={13,14,15,16};
 TFT TFTScreen;
 HMI hmi1(1,2,3,4,R,LED,TFTScreen);
-byte currentTemperature,targetTemperature,currentHumidity,targetHumidity,currentCO2,targetCO2,currentLight,targetLight=25;
-void UpdateTargetParameters()
+void UpdateTargetParameters(byte* ptrCurrentTemperature, byte targetTemperature, byte* ptrCurrentHumidity, byte targetHumidity, byte* ptrCurrentCO2, byte targetCO2,byte* ptrCurrentLight, byte targetLight)
 {
+    //current values are pointers so they can be updated while the user is changing the current value
     byte buttons=hmi1.readButtons();
     bool firstLoopIteration=true;//used by loops to check if they have just started
     byte shownTarget=0; //initialize shownTarget variable, changed when rotating the knob
+
+    float scalingTemperature=0.5;//how much should it change per step of the knob
+    float scalingHumidity=0.5;
+    float scalingCO2=0.5;
+    float scalingLight=0.5;
+
+    bool knobA,knobB=0;//created just to find the current state
+    hmi1.readRotaryEncoder(&knobA,&knobB);
+    byte encState=2*knobA+knobB;//0: 00; 1: 01; 2: 10; 3: 11; S: AB
+
     while(hmi1.readBit(buttons,0))//go to temperature menu
     {
         if(firstLoopIteration)
@@ -21,38 +31,60 @@ void UpdateTargetParameters()
             firstLoopIteration=false;
         }
 
+        byte increment =hmi1.incrementRotation(&encState);
+        shownTarget=increment*scalingTemperature;
         buttons=hmi1.readButtons();
-        hmi1.writeToScreen(currentTemperature,shownTarget);
-        if (hmi1.readBit(buttons,7)) return;
+        if (hmi1.readBit(buttons,6)) targetTemperature=shownTarget;//confirm target value
+        if (hmi1.readBit(buttons,5)) shownTarget=targetTemperature;//reset target value
+        hmi1.writeToScreen(*ptrCurrentTemperature,shownTarget);//show values
+        if (hmi1.readBit(buttons,7)) return;//exit; does not save automatically
     }
     while(hmi1.readBit(buttons,1))//go to humidity menu
     {
         if(firstLoopIteration)
         {
+            shownTarget=targetHumidity;
             firstLoopIteration=false;
         }
-        buttons=hmi1.readButtons();
 
-        if (hmi1.readBit(buttons,7)) return;
+        byte increment =hmi1.incrementRotation(&encState);
+        shownTarget=increment*scalingHumidity;
+        buttons=hmi1.readButtons();
+        if (hmi1.readBit(buttons,6)) targetHumidity=shownTarget;//confirm target value
+        if (hmi1.readBit(buttons,5)) shownTarget=targetHumidity;//reset target value
+        hmi1.writeToScreen(*ptrCurrentHumidity,shownTarget);//show values
+        if (hmi1.readBit(buttons,7)) return;//exit; does not save automatically;
     }
     while(hmi1.readBit(buttons,2))//go to CO2 menu
     {   
         if(firstLoopIteration)
         {
+            shownTarget=targetCO2;
             firstLoopIteration=false;
         }
-        buttons=hmi1.readButtons();
 
-        if (hmi1.readBit(buttons,7)) return;
+        byte increment =hmi1.incrementRotation(&encState);
+        shownTarget=increment*scalingCO2;
+        buttons=hmi1.readButtons();
+        if (hmi1.readBit(buttons,6)) targetCO2=shownTarget;//confirm target value
+        if (hmi1.readBit(buttons,5)) shownTarget=targetCO2;//reset target value
+        hmi1.writeToScreen(*ptrCurrentCO2,shownTarget);//show values
+        if (hmi1.readBit(buttons,7)) return;//exit; does not save automatically
     }
     while(hmi1.readBit(buttons,3))//go to light menu
     {
         if(firstLoopIteration)
         {
+            shownTarget=targetLight;
             firstLoopIteration=false;
         }
-        buttons=hmi1.readButtons();
 
-        if (hmi1.readBit(buttons,7)) return;
+        byte increment =hmi1.incrementRotation(&encState);
+        shownTarget=increment*scalingLight;
+        buttons=hmi1.readButtons();
+        if (hmi1.readBit(buttons,6)) targetLight=shownTarget;//confirm target value
+        if (hmi1.readBit(buttons,5)) shownTarget=targetLight;//reset target value
+        hmi1.writeToScreen(*ptrCurrentLight,shownTarget);//show values
+        if (hmi1.readBit(buttons,7)) return;//exit; does not save automatically
     }
 }
