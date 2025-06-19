c589597774a575fc45a0fdfbad6a06ad01f12193
thesemsea <isabelasanunes@gmail.com>
2025-06-19 15:51:40 +0200

    implemented mutex in LogData and UpdateTargetParameters

--- src/main.cpp
+++ src/main.cpp
@@ -161,127 +161,136 @@ void getSensorData(void *parameters)
 void UpdateTargetParameters(void *parameters)
 {
     for (;;)
-    {
-        byte buttons = hmi1.readButtons();
-        bool firstLoopIteration = true; // used by loops to check if they have just started
-        float shownTarget = 0;          // initialize shownTarget variable, changed when rotating the knob
-
-        float scalingTemperature = 0.5; // how much should it change per step of the knob
-        float scalingHumidity = 0.5;
-        float scalingCO2 = 0.5;
-        float scalingLight = 0.5;
-
-        bool knobA, knobB = 0; // created just to find the current state
-        hmi1.readRotaryEncoder(&knobA, &knobB);
-        byte encState = 2 * knobA + knobB; // 0: 00; 1: 01; 2: 10; 3: 11; S: AB
-
-        while (hmi1.readBit(buttons, 0)) // go to temperature menu
+    { // acquire the mutex before writing to shared variables
+        if (xSemaphoreTake(xSensorDataMutex, portMAX_DELAY) == pdTRUE)
         {
-            if (firstLoopIteration)
-            {
-                shownTarget = targetTemperature;
-                firstLoopIteration = false;
-            }
+            byte buttons = hmi1.readButtons();
+            bool firstLoopIteration = true; // used by loops to check if they have just started
+            float shownTarget = 0;          // initialize shownTarget variable, changed when rotating the knob
 
-            byte increment = hmi1.incrementRotation(&encState);
-            shownTarget = increment * scalingTemperature;
-            buttons = hmi1.readButtons();
-            if (hmi1.readBit(buttons, 6))
-                targetTemperature = shownTarget; // confirm target value
-            if (hmi1.readBit(buttons, 5))
-                shownTarget = targetTemperature;                 // reset target value
-            hmi1.writeToScreen(currentTemperature, shownTarget); // show values
-            if (hmi1.readBit(buttons, 7))
-                return; // exit; does not save automatically
-        }
-        while (hmi1.readBit(buttons, 1)) // go to humidity menu
-        {
-            if (firstLoopIteration)
-            {
-                shownTarget = targetHumidity;
-                firstLoopIteration = false;
-            }
+            float scalingTemperature = 0.5; // how much should it change per step of the knob
+            float scalingHumidity = 0.5;
+            float scalingCO2 = 0.5;
+            float scalingLight = 0.5;
 
-            byte increment = hmi1.incrementRotation(&encState);
-            shownTarget = increment * scalingHumidity;
-            buttons = hmi1.readButtons();
-            if (hmi1.readBit(buttons, 6))
-                targetHumidity = shownTarget; // confirm target value
-            if (hmi1.readBit(buttons, 5))
-                shownTarget = targetHumidity;                 // reset target value
-            hmi1.writeToScreen(currentHumidity, shownTarget); // show values
-            if (hmi1.readBit(buttons, 7))
-                return; // exit; does not save automatically;
-        }
-        while (hmi1.readBit(buttons, 2)) // go to CO2 menu
-        {
-            if (firstLoopIteration)
-            {
-                shownTarget = targetCO2;
-                firstLoopIteration = false;
-            }
+            bool knobA, knobB = 0; // created just to find the current state
+            hmi1.readRotaryEncoder(&knobA, &knobB);
+            byte encState = 2 * knobA + knobB; // 0: 00; 1: 01; 2: 10; 3: 11; S: AB
 
-            byte increment = hmi1.incrementRotation(&encState);
-            shownTarget = increment * scalingCO2;
-            buttons = hmi1.readButtons();
-            if (hmi1.readBit(buttons, 6))
-                targetCO2 = shownTarget; // confirm target value
-            if (hmi1.readBit(buttons, 5))
-                shownTarget = targetCO2;                 // reset target value
-            hmi1.writeToScreen(currentCO2, shownTarget); // show values
-            if (hmi1.readBit(buttons, 7))
-                return; // exit; does not save automatically
-        }
-        while (hmi1.readBit(buttons, 3)) // go to light menu
-        {
-            if (firstLoopIteration)
+            while (hmi1.readBit(buttons, 0)) // go to temperature menu
             {
-                shownTarget = targetLight;
-                firstLoopIteration = false;
-            }
+                if (firstLoopIteration)
+                {
+                    shownTarget = targetTemperature;
+                    firstLoopIteration = false;
+                }
 
-            byte increment = hmi1.incrementRotation(&encState);
-            shownTarget = increment * scalingLight;
-            buttons = hmi1.readButtons();
-            if (hmi1.readBit(buttons, 6))
-                targetLight = shownTarget; // confirm target value
-            if (hmi1.readBit(buttons, 5))
-                shownTarget = targetLight;                 // reset target value
-            hmi1.writeToScreen(currentLight, shownTarget); // show values
-            if (hmi1.readBit(buttons, 7))
-                return; // exit; does not save automatically
+                byte increment = hmi1.incrementRotation(&encState);
+                shownTarget = increment * scalingTemperature;
+                buttons = hmi1.readButtons();
+                if (hmi1.readBit(buttons, 6))
+                    targetTemperature = shownTarget; // confirm target value
+                if (hmi1.readBit(buttons, 5))
+                    shownTarget = targetTemperature;                 // reset target value
+                hmi1.writeToScreen(currentTemperature, shownTarget); // show values
+                if (hmi1.readBit(buttons, 7))
+                    return; // exit; does not save automatically
+            }
+            while (hmi1.readBit(buttons, 1)) // go to humidity menu
+            {
+                if (firstLoopIteration)
+                {
+                    shownTarget = targetHumidity;
+                    firstLoopIteration = false;
+                }
+
+                byte increment = hmi1.incrementRotation(&encState);
+                shownTarget = increment * scalingHumidity;
+                buttons = hmi1.readButtons();
+                if (hmi1.readBit(buttons, 6))
+                    targetHumidity = shownTarget; // confirm target value
+                if (hmi1.readBit(buttons, 5))
+                    shownTarget = targetHumidity;                 // reset target value
+                hmi1.writeToScreen(currentHumidity, shownTarget); // show values
+                if (hmi1.readBit(buttons, 7))
+                    return; // exit; does not save automatically;
+            }
+            while (hmi1.readBit(buttons, 2)) // go to CO2 menu
+            {
+                if (firstLoopIteration)
+                {
+                    shownTarget = targetCO2;
+                    firstLoopIteration = false;
+                }
+
+                byte increment = hmi1.incrementRotation(&encState);
+                shownTarget = increment * scalingCO2;
+                buttons = hmi1.readButtons();
+                if (hmi1.readBit(buttons, 6))
+                    targetCO2 = shownTarget; // confirm target value
+                if (hmi1.readBit(buttons, 5))
+                    shownTarget = targetCO2;                 // reset target value
+                hmi1.writeToScreen(currentCO2, shownTarget); // show values
+                if (hmi1.readBit(buttons, 7))
+                    return; // exit; does not save automatically
+            }
+            while (hmi1.readBit(buttons, 3)) // go to light menu
+            {
+                if (firstLoopIteration)
+                {
+                    shownTarget = targetLight;
+                    firstLoopIteration = false;
+                }
+
+                byte increment = hmi1.incrementRotation(&encState);
+                shownTarget = increment * scalingLight;
+                buttons = hmi1.readButtons();
+                if (hmi1.readBit(buttons, 6))
+                    targetLight = shownTarget; // confirm target value
+                if (hmi1.readBit(buttons, 5))
+                    shownTarget = targetLight;                 // reset target value
+                hmi1.writeToScreen(currentLight, shownTarget); // show values
+                if (hmi1.readBit(buttons, 7))
+                    return; // exit; does not save automatically
+            }
         }
+
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Update every 1 second
     }
 }
 void LogData(void *parameters)
 {
     for (;;)
     {
-        unsigned long time = millis(); // take the current time
-        if (timeOfLastLog > time)
-            timeOfLastLog = 0;                // if millis() overflows, reset the timeOfLastLog as well
-        if (time - timeOfLastLog > 1000 * 60) // check if 1 minute has passed since last call
+        if (xSemaphoreTake(xSensorDataMutex, portMAX_DELAY) == pdTRUE)
         {
-            timeOfLastLog = time;
-            if (posData == dataLen - 1) // array has been filled and must be shifted
+            unsigned long time = millis(); // take the current time
+            if (timeOfLastLog > time)
+                timeOfLastLog = 0;                // if millis() overflows, reset the timeOfLastLog as well
+            if (time - timeOfLastLog > 1000 * 60) // check if 1 minute has passed since last call
             {
-                for (int i = 0; i < dataLen - 1; i++) // shifted moves all data snapshots down by 1 row in the array, skips the last one since it is overwritten later
+                timeOfLastLog = time;
+                if (posData == dataLen - 1) // array has been filled and must be shifted
                 {
-                    dataLog[i][0] = dataLog[i + 1][0];
-                    dataLog[i][1] = dataLog[i + 1][1];
-                    dataLog[i][2] = dataLog[i + 1][2];
-                    dataLog[i][3] = dataLog[i + 1][3];
-                    dataLog[i][4] = dataLog[i + 1][4];
+                    for (int i = 0; i < dataLen - 1; i++) // shifted moves all data snapshots down by 1 row in the array, skips the last one since it is overwritten later
+                    {
+                        dataLog[i][0] = dataLog[i + 1][0];
+                        dataLog[i][1] = dataLog[i + 1][1];
+                        dataLog[i][2] = dataLog[i + 1][2];
+                        dataLog[i][3] = dataLog[i + 1][3];
+                        dataLog[i][4] = dataLog[i + 1][4];
+                    }
                 }
+                else
+                    posData++;                     // else increment to next pos
+                dataLog[posData][0] = time / 1000; // save current values to arrays, time is saved in seconds
+                dataLog[posData][1] = currentTemperature;
+                dataLog[posData][2] = currentHumidity;
+                dataLog[posData][3] = currentCO2;
+                dataLog[posData][4] = currentLight;
             }
-            else
-                posData++;                     // else increment to next pos
-            dataLog[posData][0] = time / 1000; // save current values to arrays, time is saved in seconds
-            dataLog[posData][1] = currentTemperature;
-            dataLog[posData][2] = currentHumidity;
-            dataLog[posData][3] = currentCO2;
-            dataLog[posData][4] = currentLight;
         }
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Update every 1 second
     }
 }
 void dataOutput(void *parameters)
@@ -315,7 +324,6 @@ void dataOutput(void *parameters)
 }
 //----------------------------------------
 
-
 void setup()
 {
     // general setup --------------------------------------------
@@ -328,23 +336,22 @@ void setup()
     }
     hmi1.drawText("test", 2, 50);
 
-    
     // setup for screen -----------------------------------------
     gfx->begin(); // initialize screen
     gfx->fillScreen(BLACK);
     gfx->setTextColor(RGB565(150, 150, 150));
     hmi1.drawText("test", 2, 50);
-    
+
     // setup for temp_hum sensor ---------------------------------
     Wire.begin();
     Wire.setClock(400000);
     tempHumSensor.begin();
-    
+
     // setup for LED strip ----------------------------------------
     strip.begin(); // INITIALIZE NeoPixel strip object
     strip.show();  // Turn OFF all pixels ASAP
     strip.setBrightness(BRIGHTNESS);
-    
+
     // random generation of data --------------------------------
     for (int i = 0; i < dataLen; i++)
     {
